17e86b9f9cf5fdd634436386b6b8c47e95b425ae
Timothy Hitchener <thitchener97@gmail.com>
2020-05-22 08:16:06 -0400

    Install Zsh and set as default shell if not present
    - Fix linting errors
    - Add comments, general cleanup

--- install.sh
+++ install.sh
@@ -5,10 +5,12 @@ readonly WORKSPACE="${HOME}/workspace"
 readonly GOPATH="${HOME}/go"
 
 function main() {
+  #Install devtools
 	if [[ ! -d "/Library/Developer/CommandLineTools" ]]; then
 		xcode-select --install
 	fi
 
+  #Install Homebrew
 	if ! [ -x "$(command -v brew)" ]; then
 		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 	fi
@@ -17,7 +19,28 @@ function main() {
 	brew bundle
 	brew cleanup
 
+  #Install ZSH
+  CHECK_ZSH_INSTALLED=$(grep /zsh$ /etc/shells | wc -l)
+
+  if [ ! "$CHECK_ZSH_INSTALLED" -ge 1 ]; then
+    echo "\033[0;33m Zsh is not installed!\033[0m"
+    echo "Installing Zsh..."
+
+    brew install zsh
+
+    sudo sh -c "echo $(which zsh) >> /etc/shells"
+
+    exit
+  fi
+
+  unset CHECK_ZSH_INSTALLED
+
+  chsh -s "$(which zsh)"
+
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 	ln -sf "${PROGDIR}/.zshrc" "${HOME}/.zshrc"
+
+
 	ln -sf "${PROGDIR}/.gitconfig" "${HOME}/.gitconfig"
 	mkdir -pv "${WORKSPACE}"
 
@@ -25,6 +48,7 @@ function main() {
 		git clone https://github.com/chriskempson/base16-shell.git "${HOME}/.config/colorschemes"
 	fi
 
+  #Nvim Setup
 	pip3 install --upgrade pip --user
 	pip3 install neovim
 	curl -fLo "${HOME}/.local/share/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
